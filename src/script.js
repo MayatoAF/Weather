e22179021452132af18c7df4321e13a380d42e97
MayatoAF <mayato.francisco@gmail.com>
2022-11-17 09:31:44 -0300

    Ajuste no código

--- src/script.js
+++ src/script.js
@@ -33,8 +33,8 @@ let weather = {
     document.querySelector(".wind").innerText =
       "Velocidade do vento: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    /*document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";*/
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
